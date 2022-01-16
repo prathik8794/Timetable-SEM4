92210472be1f7ace29cce3c5d11101bfde70073e
prathik8794 <ME20B030@iittp.ac.in>
2022-01-16 20:40:18 +0530

    bg fixed

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ var j =  Math.floor(Math.random()*256).toString(16);
 var k =  Math.floor(Math.random()*256).toString(16);
 document.getElementById('dd').style.color = '#'+i+j+k},1000);
 
-setInterval(function(){
-    var i = Math.floor(Math.random()*256).toString(16);
-    var j =  Math.floor(Math.random()*256).toString(16);
-    var k =  Math.floor(Math.random()*256).toString(16);
-    document.getElementById('hp').style.backgroundColor= '#'+i+j+k},1000);
